831b41fcbbc67c74c717fefc6f73bfd010a42204
sps-pashka-it <stetsyuk.p.s@gmail.com>
2023-05-16 15:29:59 +0300

    add fetch

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -7,61 +7,63 @@ import css from './ContactForm.module.css';
 import { toast } from 'react-toastify';
 
 import { Button } from 'components/Button/Button';
+import { getContactsApi } from 'api/fetch';
 
 export const ContactForm = () => {
-    const dispatch = useDispatch();
-    const contacts = useSelector(getContacts);
-    const handleSubmit = event => {
-        event.preventDefault();
-        const form = event.target;
-        const name = form.elements.name.value.trim();
-        const repead = contacts.find(contact => contact.name === name);
-        if (repead) {
-            // alert(`${name} is already in contacts`);
-            toast(`${name} is already in contacts`);
-            return;
-        }
-        const number = form.elements.number.value.trim();
-        const id = nanoid();
-        const contact = { id, name, number };
-        dispatch(addContacts(contact));
-        form.reset();
-    };
+  const dispatch = useDispatch();
+  const contacts = useSelector(getContacts);
 
-    const nameId = nanoid();
-    const numberId = nanoid();
+  const handleSubmit = event => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.elements.name.value.trim();
+    const repead = contacts.find(contact => contact.name === name);
+    if (repead) {
+      toast(`${name} is already in contacts`);
+      return;
+    }
+    const number = form.elements.number.value.trim();
+    const id = nanoid();
+    const contact = { id, name, number };
+    dispatch(addContacts(contact));
+    form.reset();
+    getContactsApi();
+  };
 
-    return (
-        <form className={css.form} onSubmit={handleSubmit}>
-            <label className={css.label} htmlFor={nameId}>
-                Name
-            </label>
-            <input
-                className={css.field}
-                id={nameId}
-                type="text"
-                name="name"
-                pattern="^[a-zA-Zа-яіїєґА-ЯІЇЄҐ]+(([' -][a-zA-Zа-яіїєґА-ЯІЇЄҐ ])?[a-zA-Zа-яіїєґА-ЯІЇЄҐ]*)*$"
-                title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-                placeholder="Anna Maria"
-                autoComplete="off"
-                required
-            />
-            <label className={css.label} htmlFor={numberId}>
-                Number
-            </label>
-            <input
-                className={css.field}
-                id={numberId}
-                type="tel"
-                name="number"
-                pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-                title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-                placeholder="+38 099 01 010 01"
-                autoComplete="off"
-                required
-            />
-            <Button type="submit">Add contacts</Button>
-        </form>
-    );
+  const nameId = nanoid();
+  const numberId = nanoid();
+
+  return (
+    <form className={css.form} onSubmit={handleSubmit}>
+      <label className={css.label} htmlFor={nameId}>
+        Name
+      </label>
+      <input
+        className={css.field}
+        id={nameId}
+        type="text"
+        name="name"
+        pattern="^[a-zA-Zа-яіїєґА-ЯІЇЄҐ]+(([' -][a-zA-Zа-яіїєґА-ЯІЇЄҐ ])?[a-zA-Zа-яіїєґА-ЯІЇЄҐ]*)*$"
+        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+        placeholder="Anna Maria"
+        autoComplete="off"
+        required
+      />
+      <label className={css.label} htmlFor={numberId}>
+        Number
+      </label>
+      <input
+        className={css.field}
+        id={numberId}
+        type="tel"
+        name="number"
+        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+        placeholder="+38 099 01 010 01"
+        autoComplete="off"
+        required
+      />
+      <Button type="submit">Add contacts</Button>
+    </form>
+  );
 };
